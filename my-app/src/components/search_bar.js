7ddb3b388670dbeacd94ae3e3266b8c90ddead3b
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2018-09-24 07:25:40 +0200

    15 revert from function to class component the Search bar

--- my-app/src/components/search_bar.js
+++ my-app/src/components/search_bar.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />;
-};
+// functional component
+// const SearchBar = () => {
+//     return <input />;
+// };
 
+//every class should have render
+class SearchBar extends Component {
+    render() {
+        return <input />
+    }
+}
 export default SearchBar;
