c7c1a57d34c35e159ec3635ce955051ed8825e4c
Stampoulis Spiros <35004391+Darkdream3r@users.noreply.github.com>
2018-09-24 07:55:01 +0200

    18 Using state on search bar when calling event

--- my-app/src/components/search_bar.js
+++ my-app/src/components/search_bar.js
@@ -7,16 +7,30 @@ import React, { Component } from 'react';
 
 //every class should have render
 class SearchBar extends Component {
+    constructor (props) {
+        super(props);
+        // only class component can have state
+        this.state = { term: ''};
+    }
     render() {
+        // return <input onChange={event => this.onInputChange} />;
+
         //dont use parenthesis on a sinlge value (event)
-        return <input onChange={event => console.log(event.target.value)} />;
+        // return <input onChange={event => console.log(event.target.value)} />;
+        return (
+            <div>
+            <input onChange={event => this.setState({ term: event.target.value })} />
+            Value of the input: {this.state.term}
+            </div>
+           
+        );
     }
 
-    onInputChange(event) {
+
+    // onInputChange(event) {
         //console what we type
         // console.log(event.target.value);
         // console.log(event);
-
-    }
+    // }
 }
 export default SearchBar;
